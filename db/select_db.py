0a935aaa15bcf1809fbffabf9fea6cde5448509a
lazychoi <eduroad@gmail.com>
2022-07-24 12:47:52 +0000

    feat : db에 호텔 데이터 저장하고 데이터프레임으로 불러오는 기능 구현
    csv_to_db.py : 호텔 데이터 db에 저장
    select_db.py : db 데이터를 판다스 데이터프레임으로 불러오기
    
    Issue: #3

---
+++ db/select_db.py
@@ -0,0 +1,14 @@
+import os, sys
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+
+def get_db():
+
+    url = "postgresql://postgres:postgres@localhost:5432/hoteldb"
+    engine = create_engine(url)
+
+    sql = 'SELECT * FROM hoteltb;'
+    df = pd.read_sql(sql, con=engine)
+
+    return df
