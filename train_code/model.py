aa714ed07ea52e215f4cbf62bd34cee82a451d95
jychoi <eduroad@gmail.com>
2022-07-26 10:24:18 +0900

    feat : db에 호텔 데이터 저장하고 데이터프레임으로 불러오는 기능 구현 (#4)
    csv_to_db.py : 호텔 데이터 db에 저장
    select_db.py : db 데이터를 판다스 데이터프레임으로 불러오기
    
    Issue: #3

--- train_code/model.py
+++ train_code/model.py
@@ -1,43 +1,45 @@
+import sys, os
+sys.path.append(os.pardir)
 import pandas as pd
 from arange_colums import arangeColumns
 from del_columns import deleteColumns
-from load_csv import loadCSV
+from db.select_db import get_db
 from save_model import saveModel
-from train import trianModel
+from train import trainModel
 from xgboost import XGBClassifier
 from sklearn.linear_model import LogisticRegression
+from lightgbm import LGBMClassifier
 import joblib
 
-df = loadCSV('hotel_bookings.csv')
-target = 'is_canceled'
-df, x, y = arangeColumns(df, target)
+# db에서 데이터 불러오기
+df = get_db()
+
+x = df.iloc[:,:-1]
+y = df.iloc[:,[-1]]
 
 # 결측치가 많거나 불필요해 보이는 컬럼 삭제
-del_col_names = ['agent','company','country','reservation_status_date','reservation_status', ]
+del_col_names = ['agent','company','country','reservation_status_date','reservation_status']
 x = deleteColumns(x, del_col_names)
 
 # 수치형 변수: SimpleImputer 결측치는 중앙값으로 대치 & 정규화
-numeric_features = ['adr','adults','arrival_date_day_of_month','arrival_date_week_number',
-                     'arrival_date_year','babies','booking_changes','children',
-                     'days_in_waiting_list','is_repeated_guest','lead_time',
-                     'previous_bookings_not_canceled','previous_cancellations',
-                     'required_car_parking_spaces','stays_in_week_nights',
-                     'stays_in_weekend_nights','total_of_special_requests']
+numeric_features = x.select_dtypes([int, float]).columns
 
 # 범주형 변수: 원핫인코딩
-categorical_features = ['arrival_date_month','assigned_room_type','customer_type',
-                        'deposit_type','distribution_channel','hotel','market_segment',
-                        'meal','reserved_room_type']
+categorical_features = x.select_dtypes(object).columns
 
 # xgb = XGBClassifier(booster='gbtree', eval_metric='logloss', use_label_encoder=False,
 #                    max_depth=6, learning_rate=0.1, n_estimators=100, n_jobs=-1)
-xgb = XGBClassifier()
-clf = trianModel(xgb, x, y, numeric_features, categorical_features)
-preint(x.columns)
-# logi = LogisticRegression()
-# clf = trianModel(logi, x, y, numeric_features, categorical_features)
 
+xgb = XGBClassifier()
+clf_xgb = trainModel(xgb, x, y, numeric_features, categorical_features)
+
+# logi = LogisticRegression(max_iter=1000, n_jobs=-1, solver='sag')
+# clf_logi = trainModel(logi, x, y, numeric_features, categorical_features)
+
+# lgbm = LGBMClassifier()
+# clf_lgbm = trainModel(lgbm, x, y, numeric_features, categorical_features)
 
 # 모델 저장
-# saveModel(clf, 'xgb.pkl')
-# saveModel(clf, 'logistic.pkl')
+saveModel(clf_xgb, 'xgb.pkl')
+# saveModel(clf_logi, 'logistic.pkl')
+# saveModel(clf_lgbm, 'lgbm.pkl')
--- train_code/train.py
+++ train_code/train.py
@@ -1,45 +1,36 @@
 import pandas as pd
-# import os
-
-# BASE_DIR = os.path.dirname(__file__)
-# df = pd.read_csv(f'{BASE_DIR}/../dataset/hotel_3_removeColumns.csv')
-# X = df.iloc[:,:-1]
-# y = df.iloc[:,[-1]] # 파이프라인이 2차원 행렬 요구
-
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.metrics import classification_report
-from sympy import EX
+from sklearn.metrics import classification_report, confusion_matrix
 
-def trianModel(model, x, y, numeric_features, categorical_features):
+def trainModel(model, x, y, numeric_features, categorical_features):
     # 수치형 변수: SimpleImputer 결측치는 중앙값으로 대치 & 정규화
     numeric_transformer = Pipeline(steps=[("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler())])
 
     # 범주형 변수: 원핫인코딩
     categorical_transformer = OneHotEncoder(handle_unknown="ignore")
 
-    preprocessor = ColumnTransformer(
-        transformers=[
+    ### 원핫인코딩, 결측치 대치, 정규화
+    ct = ColumnTransformer([
             ('numerical', numeric_transformer, numeric_features),
             ('categorical', categorical_transformer, categorical_features),
         ])
+    x_data = ct.fit_transform(x)
+    y_data = y.values.reshape(-1,)
+    # print(x_data.shape, y_data.shape)
 
-    # 전처리 후 모델 fit
-    clf = Pipeline( steps=[("preprocessor", preprocessor), ("classifier", model)] )
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, stratify=y_data)
+    model.fit(x_train, y_train)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y)
-    try:
-        clf.fit(x_train, y_train)
-    except Exception as e:
-        print(e)
     # 정확도 train, test
-    print("train model score: %.3f" % clf.score(x_train, y_train))
-    print("test model score: %.3f" % clf.score(x_test, y_test))
-
+    print("########## ", model.__module__, "##########")
+    print("train model score: %.3f" % model.score(x_train, y_train))
+    print("test model score: %.3f" % model.score(x_test, y_test))
+    print("\ntest confusion matrix: \n", confusion_matrix(y_test, model.predict(x_test)))
     # classification report
-    print("test Predict: \n", classification_report(y_test, clf.predict(x_test)) )
-    
-    return clf
+    print("test Predict: \n", classification_report(y_test, model.predict(x_test)) )
+
+    return model
