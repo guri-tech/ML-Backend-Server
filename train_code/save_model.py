088065079acea3218609cf8548c0999f9430ffca
lazychoi <eduroad@gmail.com>
2022-08-04 23:25:38 +0900

    feat : 필요 없는 파일 삭제
    train_code 내의 모든 코드 :  mlflow를 이용하지 않았던 코드 모두 삭제
    trained_model 내의 모든 파일 : minio를 사용함에 따라 기존에 저장했던 모델 파일 삭제
    
    Issue : #5

--- train_code/save_model.py
+++
@@ -1,7 +0,0 @@
-import os
-import joblib
-
-def saveModel(model, filename):
-    BASE_DIR = os.path.dirname(__file__)
-    file_loc = os.path.join(f'{BASE_DIR}/../trained_model/', filename)
-    joblib.dump(model, file_loc)
