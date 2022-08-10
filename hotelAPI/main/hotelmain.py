c555aa61eaab3775ae46ca559bbc2374afeaf5a7
AstroNaut000 <qhddl002@gmail.com>
2022-08-11 08:38:48 +0900

    feat-18: 파일 수정
    Issues: #18

--- hotelAPI/main/hotelmain.py
+++ hotelAPI/main/hotelmain.py
@@ -9,3 +9,6 @@ def root():
     return "호텔 예약 취소 여부 예측 분류 모델을 이용한 예측 API 생성 "
 
 app.include_router(routers.hotel_router.router)
+
+if __name__ == "__main__":
+    uvicorn.run("hotelmain:app", port=8000, reload=True)
