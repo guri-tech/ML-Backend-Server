23bb2fdab71ab20be28b7faa8c0198f6cb6491ae
윤종봉 <106497313+AstroNaut000@users.noreply.github.com>
2022-08-10 14:22:58 +0900

    Update hotel_utils.py

--- hotelAPI/utils/hotel_utils.py
+++ hotelAPI/utils/hotel_utils.py
@@ -2,10 +2,11 @@ import mlflow.pyfunc
 
 
 def load_mlflow_hotel():
+    
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
     mlflow.set_experiment("HOTEL-EXPERIMENT")
+    
     model_name = "sklearn.pipeline"
-
     hotel_clf = mlflow.pyfunc.load_model(f"models:/{model_name}/Production")
 
     return hotel_clf
