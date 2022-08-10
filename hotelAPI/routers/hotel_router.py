d3c16bd2fa63a545ce7afbeec662d9b3041c5761
AstroNaut000 <qhddl002@gmail.com>
2022-08-10 14:36:18 +0900

    feat-16: 저장 경로 수정
    같은 모델에 대한 새 버전이 더 평가가 좋아
    새 모델로 production이 될 경우 자동으로
    production이 된 모델로 추적할 수 있게
    저장 경로 지정을 변경
    
    Issues: #16
    
    Update hotel_router.py

--- hotelAPI/routers/hotel_router.py
+++ hotelAPI/routers/hotel_router.py
@@ -2,12 +2,18 @@ from fastapi import APIRouter
 
 from utils.hotel_utils import load_hotel
 from utils.dataframe import hotel_df
-from schemas.hotel_schema import IndependentVariable
+
+from schemas.hotel_schema import hotel_features
+
 router = APIRouter()
 
-@router.post("/predict")
-def predict(hotelvariable:IndependentVariable):
 
-    hotel_clf = load_hotel()
+@router.post("/predict")
+
+
+def predict(hotelvariable: hotel_features):
+
+    hotel_clf = load_mlflow_hotel()
+
     df = hotel_df(hotelvariable)
     return hotel_clf.predict(df).tolist()
--- hotelAPI/utils/hotel_utils.py
+++ hotelAPI/utils/hotel_utils.py
@@ -1,3 +1,11 @@
-from joblib import load
-def load_hotel():
-    return load('/home/jb/ML-Backend-Server/hotelAPI/xgb.pkl')
+import mlflow.pyfunc
+
+
+def load_mlflow_hotel():
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment("HOTEL-EXPERIMENT")
+    model_name = "sklearn.pipeline"
+
+    hotel_clf = mlflow.pyfunc.load_model(f"models:/{model_name}/Production")
+
+    return hotel_clf
