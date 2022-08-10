8bb94bb3b4eda2a5844d443e53abb0a175534d9f
윤종봉 <106497313+AstroNaut000@users.noreply.github.com>
2022-08-10 14:38:40 +0900

    [Feat] 저장 경로 변경 (#17)
    * feat-16: 저장 경로 수정
    
    같은 모델에 대한 새 버전이 더 평가가 좋아
    새 모델로 production이 될 경우 자동으로
    production이 된 모델로 추적할 수 있게
    저장 경로 지정을 변경
    
    Issues: #16

--- hotelAPI/routers/hotel_router.py
+++ hotelAPI/routers/hotel_router.py
@@ -3,14 +3,15 @@ from fastapi import APIRouter
 from utils.hotel_utils import load_mlflow_hotel
 from utils.dataframe import hotel_df
 from schemas.hotel_schema import hotel_features
+
 router = APIRouter()
 
+
 @router.post("/predict")
-def predict(hotelvariable:hotel_features):
+def predict(hotelvariable: hotel_features):
 
     hotel_clf = load_mlflow_hotel()
-    print(dir(hotel_clf))
+
     df = hotel_df(hotelvariable)
 
-    print(df.info())
     return hotel_clf.predict(df).tolist()
