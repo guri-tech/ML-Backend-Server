6a1b27244877143ef4ece1d9f1cb74ffa53cde97
jychoi <eduroad@gmail.com>
2022-09-07 17:52:37 +0900

    [feat-23] 새로운 데이터 입력하면 예측값, 확률, 모델 성능지표 출력하는 웹페이지 작성 (#28)
    * [feat-23] 새로운 데이터 입력 -> 예측값 출력 페이지 작성
    
    새로운 데이터 입력 페이지에서 새로운 데이터를 입력하면
    preprocessor와 model을 불러와 예측값과 예측확률을 출력
    
    
    * [feat-23] 특성 변경 및 전처리모델 Product로 저장
    
    - EDA 후 필요 없는 특성 변경
    - 전처리모델을 Product로 저장
    
    
    * [feat-23] feature 20개 선택 학습 모델
    
    - flow를 전처리와 모델 학습을 하나로 묶은 것과 분리한 것으로 구분
    - flow마다 label을 부여하여 agent를 생성
    - => 전처리와 모델을 분리한 것은 잘 작동이 되나 묶은 것은 에러 발생 중
    - => 이상하게도 삭제한 컬럼까지 전처리하여 predict할 때 에러 발생시킴
    - 전처리와 모델 학습을 분리한 flow는 mlflow default experiment에 저장
    - prefect cloud에서 잘 실행되고 schedule 지정하여 주기적 실행도 성공
    - 사용자가 데이터를 입력하면 예측 결과와 확률, 모델 성능 지표 출력하는 화면 구현
    
    
    * [feat-23] bookings.csv 데이터로 변경
    
    bookings.csv => bookingtb 테이블에 저장
    지금까지의 구현한 모든 기능 적용
    
    ISSUE #23

---
+++ add_data/train_flow.py
@@ -0,0 +1,43 @@
+from prefect import Flow, Parameter
+from prefect.run_configs import UniversalRun
+from tasks import (
+    get_data,
+    log_preprocessor,
+    set_model,
+    preprocessing,
+    train_model,
+    train_model_onepipeline,
+    log_model,
+    change_production_model,
+)
+
+
+with Flow(
+    name="Hotel_train_onePipeline", run_config=UniversalRun(labels=["prod1"])
+) as flow_pipeline:
+    eval_metric = Parameter("Evaluation Metric", "auc")
+    model_name = "xgboost_pipeline"
+    df = get_data()
+    model, params = set_model
+    trained_model, metrics = train_model_onepipeline(model, df)
+    current_version = log_model(trained_model, model_name, params, metrics, eval_metric)
+    change_production_model(model_name, current_version, eval_metric)
+
+with Flow(
+    name="Hotel_train_preprocessor", run_config=UniversalRun(labels=["prod2"])
+) as flow_with_preprocessor:
+    eval_metric = Parameter("Evaluation Metric", "auc")
+    model_name = "xgboost"
+
+    df = get_data()
+    x, y, preprocessor = preprocessing(df)
+    log_preprocessor(preprocessor)
+    model, params = set_model
+    trained_model, metrics = train_model(model, x, y)
+    current_version = log_model(trained_model, model_name, params, metrics, eval_metric)
+    change_production_model(model_name, current_version, eval_metric)
+
+if __name__ == "__main__":
+    flow_pipeline.register(project_name="hotel")
+    flow_with_preprocessor.register(project_name="hotel")
+    flow_with_preprocessor.run()
--- db/get_column_names.py
+++ db/get_column_names.py
@@ -1,13 +1,13 @@
+import os
 import pandas as pd
-import sqlalchemy
 from sqlalchemy import create_engine
+
 
 def get_column_names():
 
-    url = "postgresql://postgres:postgres@localhost:5432/hoteldb"
-    engine = create_engine(url)
+    engine = create_engine(os.getenv("DATABASE_URL"))
 
     sql = "select column_name from information_schema.columns where table_catalog = 'hoteldb' and table_name = 'hoteltb';"
     df = pd.read_sql(sql, con=engine)
-    column_names = df['column_name'].values.tolist()
+    column_names = df["column_name"].values.tolist()
     return column_names
