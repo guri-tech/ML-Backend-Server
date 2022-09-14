518e6599e97f7529e760dabdb5c3655854c8df9c
lazychoi <eduroad@gmail.com>
2022-09-14 13:18:57 +0900

    coverage.yml 추가 및 코드 수정 by black

--- hotelAPI/main/hotelmain.py
+++ hotelAPI/main/hotelmain.py
@@ -1,13 +1,17 @@
-import sys,os
+import sys, os
+
 sys.path.append(os.pardir)
 from fastapi import FastAPI
 import routers.hotel_router
 import uvicorn
 
 app = FastAPI()
+
+
 @app.get("/")
 def root():
     return "호텔 예약 취소 여부 예측 분류 모델을 이용한 예측 API 생성 "
+
 
 app.include_router(routers.hotel_router.router)
 
