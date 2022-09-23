72bb2a18312257490096cb8cf797b16e56852442
AstroNaut000 <qhddl002@gmail.com>
2022-09-23 13:42:47 +0900

    [feat]: test_api 주소 환경변수 설정
    Issues: #33

--- test_api.py
+++ test_api.py
@@ -1,4 +1,5 @@
 import json
+import os
 import pytest
 from httpx import AsyncClient
 from main import app
@@ -7,7 +8,7 @@ from main import app
 @pytest.mark.asyncio
 async def test_root():
 
-    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
+    async with AsyncClient(app=app, base_url=os.getenv("FAST_API_URI")) as ac:
         response = await ac.get("/")
         assert response.status_code == 200
 
@@ -15,7 +16,7 @@ async def test_root():
 @pytest.mark.asyncio
 async def test_read_item():
 
-    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
+    async with AsyncClient(app=app, base_url=os.getenv("FAST_API_URI")) as ac:
         response = await ac.get(
             "/",
             params={
